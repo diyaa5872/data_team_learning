63f83655d382d7817f573065dc523d055442f067
dhankhardiya.8@gmail.com <dhankhardiya.8@gmail.com>
2025-07-29 07:15:35 +0000

    Implemented functions in SQL including Scalar, aggregate functions., Joins

---
+++ databricks_Workflows/includes/configuration.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# to access the data we have to use
+# %run "../includes/configuration"
+# and use the names inside the { }
+raw_folder_path = "/mnt/vsarthidl1/raw"
+processed_folder_path = "/mnt/vsarthidl1/processed"
+demo_folder_path = "/mnt/vsarthidl1/demo"
+
+# COMMAND ----------
+
+#raw_folder_path = 'abfss://raw@vsarthidl1.dfs.core.windows.net'
+#processed_folder_path = 'abfss://processed@vsarthidl1.dfs.core.windows.net'
+#demo_folder_path = 'abfss://demo@vsarthidl1.dfs.core.windows.net'
